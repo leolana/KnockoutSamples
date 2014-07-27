6b62d127adc41e7e319394f4f556ec35845df0dc
Leonardo Lucas Lana <leolucas.lana@hotmail.com>
2014-07-27 00:52:20 -0300

    add: Adicionado a classe Modelo (ViewModel) para a tela de cadastro de cursos.

---
+++ src/ProjetoKnockout/Models/CourseFormViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoKnockout.Models
+{
+    public class CourseFormViewModel : ModelBase
+    {
+        public int CourseID { get; set; }
+
+        public string CourseName { get; set; }
+
+        public DateTime CompletionDate { get; set; }
+    }
+}
