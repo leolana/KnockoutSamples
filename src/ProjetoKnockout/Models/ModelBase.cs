7baad07e0faab43ae25f502119cac439d70b7e21
Leonardo Lucas Lana <leolucas.lana@hotmail.com>
2014-07-27 00:48:57 -0300

    add: Adicionado a classe ModelBase para centralizar principais métodos e atributos entre as classes do Modelo (ViewModel). Para que haja reutilização de código e evite tal disperdício.

---
+++ src/ProjetoKnockout/Models/ModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProjetoKnockout.Entities;
+
+namespace ProjetoKnockout.Models
+{
+    public class ModelBase
+    {
+        public DatabaseContext Context { get; set; }
+        public HttpSessionStateBase Session { get; set; }
+        public virtual void LoadDefaultValues() { }
+    }
+}
